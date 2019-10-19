38fc2eb16a782fa534dcbd199768a2f6b702d078
ElenaSha <181424@fa.ru>
2019-10-19 18:48:49 +0400

    server

--- server.py
+++ server.py
@@ -2,19 +2,28 @@ import socket
 
 sock = socket.socket()
 sock.bind(('', 9090))
-sock.listen(0)
+print("Запуск сервера")
+sock.listen(1)
+print("Начало прослушивания порта")
 conn, addr = sock.accept()
+print("iii")
 print(addr)
 
 msg = ''
 
 while True:
 	data = conn.recv(1024)
+	print("iv")
 	if not data:
 		break
 	msg += data.decode()
 	conn.send(data)
+	print("v")
 
 print(msg)
 
 conn.close()
+print("vi")
+
+print("vii")
+sock.close()
