6d50749b7afef02edd17987ea92d4851467b092c
ElenaSha <181424@fa.ru>
2019-10-20 12:18:59 +0400

    client exit

--- client.py
+++ client.py
@@ -7,10 +7,16 @@ sock.connect(('localhost', 13245))
 print("Соединение с сервером")
 
 msg = input()
-sock.send(msg.encode())
 print("Отправка данных серверу")
-data = sock.recv(1024)
+sock.send(msg.encode())
+
 print("Прием данных от сервера")
+print("Если вы хотите прервать соединение с сервером, введите \"exit\"")
+while (input() != "exit"):
+	data = sock.recv(1024)
+	print(data.decode())
+	print("Если вы хотите прервать соединение с сервером, введите \"exit\"")
+
 sock.close()
 
-print(data.decode())
+
