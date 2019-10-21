bc15a00e89553b77b350bb1678c94d0c59f04f05
ElenaSha <181424@fa.ru>
2019-10-21 21:58:57 +0400

    клиент спрашивает айпишник и порт

--- client.py
+++ client.py
@@ -3,7 +3,47 @@ from time import sleep
 
 sock = socket.socket()
 sock.setblocking(1)
-sock.connect(('localhost', 13245))
+mark = 0
+#проверка айпишника
+while mark == 0:
+	print("Введите ваш ip")
+	host = input()
+	if ("." in host) and (len(host.split(".")) == 4):
+		ip = host.split(".")
+		for i in range(4):
+			if int(ip[i])<256 and int(ip[i])>=0:
+				mark = 1;
+			else:
+				print("Ошибка ввода. Значение каждого октета должно принадлежать промежутку [0;255]")
+				print("Если вы хотите еще раз ввести ip, введите 1. Если нет - любую другую строку.")
+				ind = input()
+				if ind != "1":
+					host = 'localhost'
+					mark = 1
+				break;
+	else:
+		print("Ошибка ввода. ip-адрес должен состоять из 4 октетов, разделенных точками.")
+
+mark = 0
+#проверка порта
+while mark == 0:
+	print("Введите номер порта")
+	port = int(input())
+	if 1024<=port<=49151:
+		mark = 1
+		break
+	else:
+		print("Ошибка ввода. Номер порта должен принадлежать промежутку [1024;49151]")
+		print("Если вы хотите еще раз ввести номер порта, введите 1. Если нет - любую другую строку.")
+		ind = input()
+		if ind != "1":
+			port = 1025
+			mark = 1
+
+print("ATTENTION!!!!!!")
+print(host, " ", port)
+
+sock.connect((host, port))
 print("Соединение с сервером")
 
 msg = input()
@@ -16,7 +56,6 @@ while (input() != "exit"):
 	data = sock.recv(1024)
 	print(data.decode())
 	print("Если вы хотите прервать соединение с сервером, введите \"exit\"")
-sock.send("stop".encode())
 sock.close()
 
 
